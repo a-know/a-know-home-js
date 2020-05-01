e35b3830cd2b9171ceed0dcb124db131b186606c
a-know <a.know.dev@gmail.com>
2020-05-01 09:31:14 +0900

    import assets

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/bootstrap.min.css'
+import '../styles/font-awesome.css'
+import '../styles/magnific-popup.css'
+import '../styles/template.css'
+import '../styles/themify-icons.css'
+import '../styles/vertical.min.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
